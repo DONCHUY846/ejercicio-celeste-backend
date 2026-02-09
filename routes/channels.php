48fb6061ec91594e23e591dedea27ade6e23ec02
jesusavina-commits <jesus.avina@roii.tech>
2026-02-08 18:31:06 -0600

    feat: ad more segurity to the channels

--- routes/channels.php
+++ routes/channels.php
@@ -4,8 +4,8 @@ use Illuminate\Support\Facades\Broadcast;
 
 Broadcast::channel('App.Models.Usuario.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
-});
+}, ['guards' => ['sanctum']]);
 
 Broadcast::channel('surveys.{id}', function ($user, $id) {
     return true; // Everyone authenticated can listen
-});
+}, ['guards' => ['sanctum']]);
